5da1a4b1ecf503c3875da92c30221eba4d5691b7
coolbeatz71 <sigmacool@gmail.com>
2021-04-11 02:52:32 +0200

    chore(setup): add the husky pre-commit checker

--- build/interfaces/provider.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var EProvider;
-(function (EProvider) {
-    EProvider["LOCAL"] = "LOCAL";
-    EProvider["FACEBOOK"] = "FACEBOOK";
-    EProvider["GOOGLE"] = "GOOGLE";
-})(EProvider || (EProvider = {}));
-exports.default = EProvider;
