5be6df6eb5aa90d8c934b839139fac9c38ba0c2b
coolbeatz71 <sigmacool@gmail.com>
2021-04-24 17:20:28 +0200

    feat(video-feed): Implement reading the videos feed by the user

--- src/api/routes/video.ts
+++ src/api/routes/video.ts
@@ -4,6 +4,7 @@ import videoCtrl from '../controllers/video';
 const router = express.Router();
 
 router.get('/', videoCtrl.getAll);
+router.get('/feed', videoCtrl.getFeed);
 router.get('/:slug', videoCtrl.get);
 
 export default router;
