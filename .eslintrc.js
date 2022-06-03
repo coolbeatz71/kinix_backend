85d3f5e7010ae40f8c17e8e19d4911bd4454de2a
coolbeatz71 <sigmacool@gmail.com>
2022-06-03 14:04:05 +0200

    feat: implement admin authentication (login, get currentUser, signout)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ['./node_modules/poetic/config/eslint/eslint-config.js'],
   rules: {
+    'arrow-body-style': ['off', 'as-needed'],
     '@typescript-eslint/explicit-member-accessibility': 0,
     '@typescript-eslint/explicit-function-return-type': 0,
     '@typescript-eslint/no-parameter-properties': 0,
@@ -12,6 +13,7 @@ module.exports = {
     {
       files: ['*.ts'],
       rules: {
+        'arrow-body-style': ['off', 'as-needed'],
         'no-shadow': 'off',
         'no-param-reassign': [2, { props: false }],
         'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }],
--- src/db/config.ts
+++ src/db/config.ts
@@ -4,25 +4,23 @@ import { Options } from 'sequelize';
 
 config();
 
-const getOptions = (env: string, options?: Partial<Options>): Options => {
-  return {
-    username: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    database: process.env[`PGDATABASE_${env}`],
-    host: process.env.PGHOST,
-    dialect: 'postgres',
-    // eslint-disable-next-line no-console
-    logging: ['DEV', 'TEST'].includes(env) ? console.log : false,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-        ca: fs.readFileSync(`${__dirname}/../../us-east-1-bundle.pem`),
-      },
+const getOptions = (env: string, options?: Partial<Options>): Options => ({
+  username: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env[`PGDATABASE_${env}`],
+  host: process.env.PGHOST,
+  dialect: 'postgres',
+  // eslint-disable-next-line no-console
+  logging: ['DEV', 'TEST'].includes(env) ? console.log : false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+      ca: fs.readFileSync(`${__dirname}/../../us-east-1-bundle.pem`),
     },
-    ...options,
-  };
-};
+  },
+  ...options,
+});
 
 export const test = getOptions('TEST');
 export const production = getOptions('PROD');
