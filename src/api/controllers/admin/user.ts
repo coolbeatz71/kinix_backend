b2db539ebc71abda0b005b8910a1a8944ebe1923
coolbeatz71 <sigmacool@gmail.com>
2022-08-13 11:34:58 +0200

    feat: add the routes to block, unblock and delete users

--- src/api/controllers/admin/user.ts
+++ src/api/controllers/admin/user.ts
@@ -1,9 +1,28 @@
 /* eslint-disable consistent-return */
 import { Request, Response } from 'express';
-import { OK } from 'http-status';
+import { CONFLICT, FORBIDDEN, NOT_FOUND, OK, UNAUTHORIZED } from 'http-status';
 import { Op } from 'sequelize';
+import {
+  PASSWORD_INVALID,
+  PASSWORD_REQUIRED,
+  USERNAME_EMAIL_INVALID,
+  USER_ALREADY_ACTIVE,
+  USER_ALREADY_BLOCKED,
+  USER_BLOCKED_SUCCESS,
+  USER_DELETED_SUCCESS,
+  USER_NOT_FOUND,
+  USER_UNBLOCKED_SUCCESS,
+} from '../../../constants/message';
 import db from '../../../db/models';
-import { getPagination, getPagingData, getResponse, getServerError } from '../../../helpers/api';
+import {
+  comparePassword,
+  contentResponse,
+  getPagination,
+  getPagingData,
+  getResponse,
+  getServerError,
+} from '../../../helpers/api';
+import { IJwtPayload } from '../../../interfaces/api';
 import ERole from '../../../interfaces/role';
 
 export class AdminUser {
@@ -86,6 +105,197 @@ export class AdminUser {
 
       return getResponse(res, OK, {
         data: result,
+      });
+    } catch (error) {
+      return getServerError(res, error.message);
+    }
+  };
+
+  /**
+   * controller to unblock a user
+   * @param req Request
+   * @param res Response
+   */
+  unblock = async (req: Request, res: Response): Promise<any> => {
+    const { id } = req.params;
+    const { password } = req.body;
+    const { email } = req.user as IJwtPayload;
+
+    if (!password) {
+      return getResponse(res, UNAUTHORIZED, {
+        message: PASSWORD_REQUIRED,
+      });
+    }
+
+    try {
+      const admin = await db.User.findOne({
+        where: {
+          email,
+        },
+      });
+      if (!admin) {
+        return getResponse(res, UNAUTHORIZED, {
+          message: USERNAME_EMAIL_INVALID,
+        });
+      }
+
+      const isPasswordValid = comparePassword(password, admin.get().password);
+
+      if (!isPasswordValid) {
+        return getResponse(res, FORBIDDEN, {
+          message: PASSWORD_INVALID,
+        });
+      }
+
+      const user = await db.User.findOne({
+        where: {
+          id,
+        },
+      });
+
+      if (!user) {
+        return getResponse(res, NOT_FOUND, {
+          message: USER_NOT_FOUND,
+        });
+      }
+
+      if (user.get().active) {
+        return getResponse(res, CONFLICT, {
+          message: USER_ALREADY_ACTIVE,
+        });
+      }
+
+      const update = await db.User.update(
+        {
+          active: true,
+        },
+        { where: { id }, returning: true },
+      );
+
+      return contentResponse(res, update[1][0], OK, USER_UNBLOCKED_SUCCESS);
+    } catch (error) {
+      return getServerError(res, error.message);
+    }
+  };
+
+  /**
+   * controller to block a user
+   * @param req Request
+   * @param res Response
+   */
+  block = async (req: Request, res: Response): Promise<any> => {
+    const { id } = req.params;
+    const { password } = req.body;
+    const { email } = req.user as IJwtPayload;
+
+    if (!password) {
+      return getResponse(res, UNAUTHORIZED, {
+        message: PASSWORD_REQUIRED,
+      });
+    }
+
+    try {
+      const admin = await db.User.findOne({
+        where: {
+          email,
+        },
+      });
+      if (!admin) {
+        return getResponse(res, UNAUTHORIZED, {
+          message: USERNAME_EMAIL_INVALID,
+        });
+      }
+
+      const isPasswordValid = comparePassword(password, admin.get().password);
+
+      if (!isPasswordValid) {
+        return getResponse(res, FORBIDDEN, {
+          message: PASSWORD_INVALID,
+        });
+      }
+
+      const user = await db.User.findOne({
+        where: {
+          id,
+        },
+      });
+
+      if (!user) {
+        return getResponse(res, NOT_FOUND, {
+          message: USER_NOT_FOUND,
+        });
+      }
+
+      if (!user.get().active) {
+        return getResponse(res, CONFLICT, {
+          message: USER_ALREADY_BLOCKED,
+        });
+      }
+
+      const update = await db.User.update(
+        {
+          active: false,
+        },
+        { where: { id }, returning: true },
+      );
+
+      return contentResponse(res, update[1][0], OK, USER_BLOCKED_SUCCESS);
+    } catch (error) {
+      return getServerError(res, error.message);
+    }
+  };
+
+  /**
+   * controller to delete a user from the DB
+   * @param req Request
+   * @param res Response
+   */
+  delete = async (req: Request, res: Response): Promise<any> => {
+    const { id } = req.params;
+    const { password } = req.body;
+    const { email } = req.user as IJwtPayload;
+
+    if (!password) {
+      return getResponse(res, UNAUTHORIZED, {
+        message: PASSWORD_REQUIRED,
+      });
+    }
+
+    try {
+      const admin = await db.User.findOne({
+        where: {
+          email,
+        },
+      });
+      if (!admin) {
+        return getResponse(res, UNAUTHORIZED, {
+          message: USERNAME_EMAIL_INVALID,
+        });
+      }
+
+      const isPasswordValid = comparePassword(password, admin.get().password);
+
+      if (!isPasswordValid) {
+        return getResponse(res, FORBIDDEN, {
+          message: PASSWORD_INVALID,
+        });
+      }
+
+      const user = await db.User.findOne({
+        where: {
+          id,
+        },
+      });
+
+      if (!user) {
+        return getResponse(res, NOT_FOUND, {
+          message: USER_NOT_FOUND,
+        });
+      }
+
+      await db.User.destroy({ where: { id } });
+      return getResponse(res, OK, {
+        message: USER_DELETED_SUCCESS,
       });
     } catch (error) {
       return getServerError(res, error.message);
