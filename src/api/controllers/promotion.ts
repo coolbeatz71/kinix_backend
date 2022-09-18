962914868496f0b99b0980d98dc8d0fba4278aaf
coolbeatz71 <sigmacool@gmail.com>
2022-09-18 03:38:31 +0200

    feat: implement the endpoint to get ads/stories

--- src/api/controllers/promotion.ts
+++ src/api/controllers/promotion.ts
@@ -1,7 +1,19 @@
 import { Request, Response } from 'express';
 import { OK } from 'http-status';
 import db from '../../db/models';
+import Ads from '../../db/models/ads';
+import Story from '../../db/models/story';
 import { getPagination, getPagingData, getResponse, getServerError } from '../../helpers/api';
+import {
+  getBasicAds,
+  getBasicStory,
+  getFreeAds,
+  getFreeStory,
+  getPremiumAds,
+  getPremiumStory,
+  getProfessionalAds,
+  getProfessionalStory,
+} from '../../helpers/promotion';
 
 export class Promotion {
   /**
@@ -50,6 +62,50 @@ export class Promotion {
 
       return getResponse(res, OK, {
         data: result,
+      });
+    } catch (error) {
+      return getServerError(res, error.message);
+    }
+  };
+
+  /**
+   * controller to get the ads to be displayed
+   * @param req Request
+   * @param res Response
+   */
+  getAllAds = async (_req: Request, res: Response): Promise<Response> => {
+    try {
+      const freeAds = (await getFreeAds(res)) as Ads[];
+      const basicAds = (await getBasicAds(res)) as Ads[];
+      const premiumAds = (await getPremiumAds(res)) as Ads[];
+      const professionalAds = (await getProfessionalAds(res)) as Ads[];
+
+      const ads = [...premiumAds, ...professionalAds, ...basicAds, ...freeAds];
+
+      return getResponse(res, OK, {
+        data: ads,
+      });
+    } catch (error) {
+      return getServerError(res, error.message);
+    }
+  };
+
+  /**
+   * controller to get the story to be displayed
+   * @param req Request
+   * @param res Response
+   */
+  getAllStory = async (_req: Request, res: Response): Promise<Response> => {
+    try {
+      const freeStory = (await getFreeStory(res)) as Story[];
+      const basicStory = (await getBasicStory(res)) as Story[];
+      const premiumStory = (await getPremiumStory(res)) as Story[];
+      const professionalStory = (await getProfessionalStory(res)) as Story[];
+
+      const story = [...premiumStory, ...professionalStory, ...basicStory, ...freeStory];
+
+      return getResponse(res, OK, {
+        data: story,
       });
     } catch (error) {
       return getServerError(res, error.message);
--- src/api/routes/promotion.ts
+++ src/api/routes/promotion.ts
@@ -6,4 +6,7 @@ const router = express.Router();
 router.get('/plan/ads', promotionCtrl.getAllAdsPlans);
 router.get('/plan/story', promotionCtrl.getAllStoryPlans);
 
+router.get('/ads', promotionCtrl.getAllAds);
+router.get('/story', promotionCtrl.getAllStory);
+
 export default router;
