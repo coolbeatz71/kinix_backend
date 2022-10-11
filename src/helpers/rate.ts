427a638d5ac1201743f54c8bc83ca6d995ebb958
coolbeatz71 <sigmacool@gmail.com>
2022-10-11 23:14:27 +0200

    bug: fix countRate issue by adding videoId to the query

--- src/helpers/rate.ts
+++ src/helpers/rate.ts
@@ -1,11 +1,12 @@
 import { Response } from 'express';
+import { Op } from 'sequelize';
 import db from '../db/models';
 import { getServerError } from './api';
 
-const countRate = async (res: Response, value: number): Promise<any> => {
+const countRate = async (res: Response, videoId: number, value: number): Promise<any> => {
   try {
     const result = await db.Rate.count({
-      where: { count: value },
+      where: { [Op.and]: [{ count: value }, { videoId }] },
     });
 
     return result;
